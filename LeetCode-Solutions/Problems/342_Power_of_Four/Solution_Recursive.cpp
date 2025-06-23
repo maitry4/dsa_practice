eba7a51cfb2860a45820ca9175bd741b942383ba
Maitry Parikh <maitryparikh23@gmail.com>
2025-06-23 15:31:05 +0530

    Create Solution_Recursive.cpp

---
+++ LeetCode-Solutions/Problems/342_Power_of_Four/Solution_Recursive.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(double n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n < 1) {
+            return false;
+        }
+        return isPowerOfFour(n / 4.0);
+    }
+};
